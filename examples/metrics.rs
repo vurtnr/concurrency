367fcb4828edb7483059e0fafdfea59c47cbb13e
yumin.jiao <yumin.jiao@trinasolar.com>
2024-05-29 06:58:06 +0800

    features: metrics support

---
+++ examples/metrics.rs
@@ -0,0 +1,56 @@
+use anyhow::Result;
+use concurrency::Metrics;
+use core::task;
+use rand::{random, thread_rng, Rng};
+use std::{thread, time::Duration};
+
+const N: usize = 2;
+const M: usize = 4;
+
+fn main() -> Result<()> {
+    let metrics = Metrics::new();
+
+    println!("{:?}", metrics.snapshot());
+
+    for idx in 0..N {
+        task_worker(idx, metrics.clone())?;
+    }
+
+    for idx in 0..M {
+        request_worker(metrics.clone())?;
+    }
+
+    loop {
+        thread::sleep(Duration::from_secs(2));
+        println!("{:?}", metrics.snapshot());
+    }
+
+    Ok(())
+}
+
+fn task_worker(idx: usize, metrics: Metrics) -> Result<()> {
+    thread::spawn(move || {
+        loop {
+            let mut rng: rand::prelude::ThreadRng = thread_rng();
+            thread::sleep(Duration::from_millis(rng.gen_range(100..5000)));
+            metrics.inc(format!("call.thread.worker.{}", idx))?;
+        }
+        #[allow(unreachable_code)]
+        Ok::<_, anyhow::Error>(())
+    });
+    Ok(())
+}
+
+fn request_worker(metrics: Metrics) -> Result<()> {
+    thread::spawn(move || {
+        loop {
+            let mut rng = rand::thread_rng();
+            thread::sleep(Duration::from_millis(rng.gen_range(50..800)));
+            let page = rng.gen_range(1..5);
+            metrics.inc(format!("req.page.{}", page))?;
+        }
+        #[allow(unreachable_code)]
+        Ok::<_, anyhow::Error>(())
+    }); 
+    Ok(())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 mod matriix;  
 mod vector;
+mod metrics;
 
 pub use matriix::{Matrix, multiply};
 pub use vector::{Vector, dot_product};
+pub use metrics::Metrics;
